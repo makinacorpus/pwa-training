e66232194904797766ecec9349d5ab73a96b0acd
Alexandra Janin <contact.ajanin@gmail.com>
2018-03-16 16:14:59 +0100

    Ajout des ressources statiques en cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,35 @@
+var filesToCache = [
+  // Ressources locales
+  '/',
+  '/bundle.js',
+  '/style.css',
+  '/images/icon.png',
+  '/images/news.svg',
+  '/mdl/material.min.css',
+  '/mdl/material.min.js',
+
+  // Ressources externes
+  'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
+
 self.addEventListener("install", event => {
   console.log('Service Worker installé');
+  event.waitUntil(
+    // On Install
+    caches.open('nr-v1').then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
-  // TODO : lister les ressources statiques
-  // et les ajouter au cache
-  
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // Cache First
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
